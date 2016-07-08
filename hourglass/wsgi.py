f5b4abcf16917519c5ec540245365e71b8babc57
James Seppi <james.seppi@gsa.gov>
2016-07-08 16:11:25 -0500

    rm whitenoise, add dj-static, cc @toolness

--- hourglass/wsgi.py
+++ hourglass/wsgi.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hourglass.settings")
 try:
-    application = get_wsgi_application()
+    application = Cling(get_wsgi_application())
 except Exception as e:
     print(e)
